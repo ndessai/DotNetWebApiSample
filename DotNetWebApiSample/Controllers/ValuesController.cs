430718cbd69f1569aedcde14946ba55a4f9a39a3
ndessai <ndessai@gmail.com>
2017-12-11 23:42:31 -0500

    revert again

--- DotNetWebApiSample/Controllers/ValuesController.cs
+++ DotNetWebApiSample/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace DotNetWebApiSample.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value1";
+            return "value";
         }
 
         // POST api/values
